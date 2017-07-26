66eb96d471ed97668d07700bc33ccfe87fd761ae
SixZero <havliktomi@hotmail.com>
2017-07-26 18:33:28 +0200

    Some min

--- datacollectors/ccxt_collector.py
+++ datacollectors/ccxt_collector.py
@@ -1,23 +1,37 @@
 import ccxt
+import os
+import json
+from pprint import pprint
 
+with open('exchange.keys') as data_file:
+    keys = json.load(data_file)
+# pprint(keys)
+
+
+API_KEY = keys.get('POLONIEX_API_KEY', None)
+print(API_KEY)
+API_SECRET = keys.get('POLONIEX_API_SECRET', None)
+print(API_SECRET)
 
 hitbtc = ccxt.hitbtc ({ 'verbose': True })
 bitmex = ccxt.bitmex ()
-huobi  = ccxt.huobi ()
+huobi  = ccxt.poloniex ({
+    'apiKey': API_KEY,
+    'secret': API_SECRET,}
+)
 # exmo   = ccxt.exmo ({
 #     'apiKey': 'YOUR_PUBLIC_API_KEY',
 #     'secret': 'YOUR_SECRET_PRIVATE_KEY',
 # })
 
-hitbtc_products = hitbtc.load_products ()
 
-print (hitbtc.id, hitbtc_products)
-print (bitmex.id, bitmex.load_products ())
-print (huobi.id,  huobi.load_products ())
+# print (hitbtc.id, hitbtc.load_products ())
+# print (bitmex.id, bitmex.load_products ())
+# print (huobi.id,  huobi.load_products ())
 
-print (hitbtc.fetch_order_book (hitbtc.symbols[0]))
-print (bitmex.fetch_ticker ('BTC/USD'))
-print (huobi.fetch_trades ('LTC/CNY'))
+# print (hitbtc.fetch_order_book (hitbtc.symbols[0]))
+# print (bitmex.fetch_ticker ('BTC/USD'))
+# print (huobi.fetch_trades ('LTC/CNY'))
 
 # print (exmo.fetch_balance ())
 
